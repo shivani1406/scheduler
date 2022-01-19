6a8599a230373ac082ebcee9b668265e03867374
Shivani Sharma <shivani1406sharma@gmail.com>
2022-01-19 14:45:22 -0500

    added InterviewerListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,7 +6,7 @@ import classNames from "classnames";
 export default function DayListItem(props) {
   const dayListItemClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots
+    "day-list__item--full": props.spots===0
   });
 
   const formatSpots = (spot) => {
