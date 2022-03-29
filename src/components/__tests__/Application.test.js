cbb5f09bc2c8da3247124d51663c305e1c6a7e1a
Shivani Sharma <shivani1406sharma@gmail.com>
2022-03-29 14:59:04 -0400

    added testing code

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,22 +1,15 @@
 import React from "react";
+import { render, cleanup, waitForElement, wait, fireEvent, queryByAltText, getByText, getByAltText, getAllByTestId, prettyDOM, getByPlaceholderText, queryByText, queryAllByAltText } from "@testing-library/react";
 
-import {
-  render,
-  cleanup,
-  waitForElement,
-  getByText,
-  getAllByTestId,
-  getByAltText,
-  getByPlaceholderText,
-  queryByText,
-  queryByAltText,
-  getByDisplayValue
-} from '@testing-library/react';
-
+import axios from 'axios';
 import Application from "components/Application";
+import Empty from "components/Appointment/Empty";
+
+beforeEach(() => {
+  jest.resetModules();
+});
 
 afterEach(cleanup);
-
 it("renders without crashing", () => {
   render(<Application />);
 });
